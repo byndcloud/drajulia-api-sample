64c3afe302f5eb41030ada54c2e94dc622e82385
Arthur Lapprand <lapprand@gmail.com>
2020-02-17 17:57:24 -0300

    feat: add better loading states

--- src/components/Models.vue
+++ src/components/Models.vue
@@ -7,7 +7,7 @@
       v-col.d-flex.justify-center()
         v-fade-transition(group hide-on-leave)
           v-skeleton-loader.mb-4(v-if='loadingModels' v-for='i in 3' type='list-item, actions' min-width='200' width='90vw' :key='i' elevation='1')
-          template(v-if='!loadingModels' v-for='model in models')
+          template(v-if='!loadingModels' v-for='(model, index) in models')
             v-card.mb-4(:key='model.uid')
               v-card-text
                 v-icon(left) far fa-file
@@ -16,7 +16,7 @@
               //-   span(v-html="model.bodyDescription")
               v-card-actions
                 v-spacer
-                v-btn(@click='createDoc(model)' :loading='creatingDoc' color='primary' small) Criar documento
+                v-btn(@click='createDoc(model, index)' :disabled='creatingDoc && !btnLoadingStates[index]' :loading='btnLoadingStates[index]' color='primary' small) Criar documento
 </template>
 
 <script>
@@ -27,19 +27,23 @@ export default {
   data: () => ({
     models: [],
     loadingModels: true,
-    creatingDoc: false
+    creatingDoc: false,
+    btnLoadingStates: []
   }),
   async created () {
     const response = await this.$axios.get('/public/documentmodels')
     this.models = response.data
+    this.models.forEach(m => this.btnLoadingStates.push(false))
     this.loadingModels = false
   },
   computed: {
     ...mapFields('sample', ['embedLink'])
   },
   methods: {
-    async createDoc (model) {
+    async createDoc (model, index) {
       this.creatingDoc = true
+      this.btnLoadingStates[index] = true
+      this.$forceUpdate()
       const response = await this.$axios.post(
         '/embedding/createdoc',
         {
@@ -54,7 +58,9 @@ export default {
         this.embedLink = response.data.embedLink
         this.$router.push('doc')
       }
+      this.btnLoadingStates[index] = false
       this.creatingDoc = false
+      this.$forceUpdate()
     }
   }
 }
